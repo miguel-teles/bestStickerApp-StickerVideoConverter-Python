94893f15418ab48ace7844766e004062a3972533
miguel <miguelteles2002@gmail.com>
2026-05-26 23:37:48 -0300

    testando velocidade da conversão

--- src/app.py
+++ src/app.py
@@ -1,16 +1,19 @@
 import json
 import os
+from datetime import datetime
 
 import boto3
 import subprocess
 import logging
 
 OUTPUT_BUCKET_NAME = os.environ["OUTPUT_BUCKET_NAME"]
+FFMPEG_PATH = "/opt/bin/ffmpeg"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    initial_date = datetime.now()
     try:
         key = event.get("Records")[0].get("s3").get("object").get("key")
         bucket = event.get("Records")[0].get("s3").get("bucket").get("name")
@@ -51,23 +54,33 @@ def lambda_handler(event, context):
     except Exception as e:
         print("ERROR:", str(e))
         raise e
+    finally:
+        final_date = datetime.now()
+        execution_duration = (final_date - initial_date).total_seconds()
+        logger.info("Processing duration: {}s".format(execution_duration))
+
 
 
 def create_command(input_file, output_file):
     return [
-        "ffmpeg",
+        FFMPEG_PATH,
+        "-y",
         "-i", input_file,
 
-        # limita a duração pra 10 segundos
+        # max duration
         "-t", "10",
 
-        # Resize (optional)
-        "-vf", "scale=512:512:force_original_aspect_ratio=decrease,fps=15",
+        # resize + lower fps
+        "-vf", "scale=512:512:force_original_aspect_ratio=decrease,fps=12",
 
-        # Better animated webp settings
+        # animated webp settings
         "-loop", "0",
-        "-preset", "default",
         "-an",
+
+        # webp encoder tuning
+        "-lossless", "0",
+        "-quality", "30",
+        "-compression_level", "0",
 
         output_file
     ]
